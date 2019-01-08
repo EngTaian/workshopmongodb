3a2f08b0491733f3d814ef51b203f19867d4f774
EngTaian <taian.silva@hotmail.com>
2019-01-08 12:42:56 -0200

    Obtendo usuario por id com DTO

--- src/main/java/com/taian/worshopmongo/services/UserService.java
+++ src/main/java/com/taian/worshopmongo/services/UserService.java
@@ -1,19 +1,29 @@
 package com.taian.worshopmongo.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.List;
+
 import com.taian.worshopmongo.domain.User;
 import com.taian.worshopmongo.repository.UserRepository;
+import com.taian.worshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
+	public User findById(String id) {
+		
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+
+	}
 }
