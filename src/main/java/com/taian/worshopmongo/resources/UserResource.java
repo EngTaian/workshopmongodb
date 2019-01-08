33e31cf5d251f4d6ebf93d32dc1214c763eeb4bd
EngTaian <taian.silva@hotmail.com>
2019-01-08 12:18:37 -0200

    Retornando dados no padrão DTO

--- src/main/java/com/taian/worshopmongo/resources/UserResource.java
+++ src/main/java/com/taian/worshopmongo/resources/UserResource.java
@@ -1,16 +1,17 @@
 package com.taian.worshopmongo.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import com.taian.worshopmongo.domain.User;
-import com.taian.worshopmongo.services.UserService;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import com.taian.worshopmongo.domain.User;
+import com.taian.worshopmongo.dto.UserDTO;
+import com.taian.worshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value="/users")	
@@ -20,9 +21,10 @@ public class UserResource {
 	private UserService service;
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = service.findAll();		
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDTO = list.stream().map(user -> new UserDTO(user)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
 	}
 	
 }
