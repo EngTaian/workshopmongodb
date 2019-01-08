b14bf8ea6cdb31317e9d03b425f66c36a62d93f4
EngTaian <taian.silva@hotmail.com>
2019-01-08 14:18:48 -0200

    User aninhado

---
+++ src/main/java/com/taian/worshopmongo/repository/PostRepository.java
@@ -0,0 +1,14 @@
+package com.taian.worshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.taian.worshopmongo.domain.Post;
+
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+	
+	
+
+}
