6d340cc5c713bef039f16bb88033af7de6e6ac96
EngTaian <taian.silva@hotmail.com>
2019-01-08 17:33:46 -0200

    Consulta com vários critérios

--- src/main/java/com/taian/worshopmongo/services/PostService.java
+++ src/main/java/com/taian/worshopmongo/services/PostService.java
@@ -1,5 +1,7 @@
 package com.taian.worshopmongo.services;
 
+
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -33,5 +35,10 @@ public class PostService {
 	
 	public List<Post> findByTitleJSon(String title){
 		return repo.findByTitle(title);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getDate() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
 	}
 }
