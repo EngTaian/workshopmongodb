a787dc41dfccc623b1eaa94272ec70396d12687e
EngTaian <taian.silva@hotmail.com>
2019-01-08 14:28:13 -0200

    Projeção dos dados do autor com DTO

--- src/main/java/com/taian/worshopmongo/config/Instantiation.java
+++ src/main/java/com/taian/worshopmongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.taian.worshopmongo.domain.Post;
 import com.taian.worshopmongo.domain.User;
+import com.taian.worshopmongo.dto.AuthorDTO;
 import com.taian.worshopmongo.repository.PostRepository;
 import com.taian.worshopmongo.repository.UserRepository;
 
@@ -30,14 +31,16 @@ public class Instantiation implements CommandLineRunner {
 		
 		userRepository.deleteAll();
 		
-		User maria = new User(null, "Maria Brown", "maria@gmail.com");
-		User alex = new User(null, "Alex Green", "alex@gmail.com");
-		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
-		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		User maria = new User(null, "maria@gmail.com", "Maria Brown");
+		User alex = new User(null, "alex@gmail.com", "Alex Green");
+		User bob = new User(null, "bob@gmail.com", "Bob Grey");
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 	
